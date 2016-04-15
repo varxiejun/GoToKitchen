313640798949ea046ac04cbd9cf0888dbf0c2a1e
看楼听雨 <273794982@qq.com>
2016-04-16 01:51:55 +0800

    Initial Commit

---
+++ GoToKitchen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoToKitchen
+//
+//  Created by 看楼听雨 on 16/4/16.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoToKitchen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoToKitchen
+//
+//  Created by 看楼听雨 on 16/4/16.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
